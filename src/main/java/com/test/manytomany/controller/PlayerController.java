2f451c3253b2db8c09319957ce48f444ac8b16f8
Matiu-dev <matiw97@wp.pl>
2022-02-28 10:19:28 +0100

    add basic registration

--- src/main/java/com/test/manytomany/controller/PlayerController.java
+++ src/main/java/com/test/manytomany/controller/PlayerController.java
@@ -1,21 +1,34 @@
 package com.test.manytomany.controller;
 
 import com.test.manytomany.model.player.Player;
+import com.test.manytomany.model.player.PlayerRole;
 import com.test.manytomany.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@CrossOrigin
-@RequestMapping("/player")
+//@RequestMapping("/player")
+@Controller
 public class PlayerController {
+
     @Autowired
     private PlayerService playerService;
 
-    @PostMapping("/create")
-    public ResponseEntity<?> savePlayer(@RequestBody Player player) {
-        return new ResponseEntity<>(playerService.savePlayer(player), HttpStatus.CREATED);
+//    @GetMapping("/createPlayer")
+//    public String savePlayer(Model model) {
+//        System.out.println("test");
+////        return new ResponseEntity<>(playerService.savePlayer(player), HttpStatus.CREATED);
+//        return "createPlayer";
+//    }
+
+    @PostMapping("/createPlayer")
+    public String greetingSubmit(@ModelAttribute Player player, Model model) {
+        player.setPlayerRole(PlayerRole.ROLE_USER);
+        playerService.savePlayer(player);
+
+        return "login";
     }
 }
